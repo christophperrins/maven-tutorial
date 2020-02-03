3986733228787d6af4a96632bfb3b97d4ea068b1
Chris <chris>
2020-02-03 14:39:16 +0000

    core work

---
+++ src/main/java/com/qa/tutorial/Interaction.java
@@ -0,0 +1,20 @@
+package com.qa.tutorial;
+
+public class Interaction {
+
+	/**
+	 * This method is for people you like
+	 * @return a greeting
+	 */
+	public String greeting() {
+		return "Hi friend";
+	}
+	
+	/**
+	 * This method is for people in your class
+	 * @return an insult
+	 */
+	public String insult() {
+		return "Is trash";
+	}
+}
---
+++ src/main/java/com/qa/tutorial/Runner.java
@@ -0,0 +1,10 @@
+package com.qa.tutorial;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Interaction potatoes = new Interaction();
+		System.out.println(potatoes.greeting() + " " + potatoes.insult());
+	}
+
+}
